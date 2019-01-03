4505ae1c0cc567dcac75ea32d7100bab2fc656fe
evolfo <spampinatodesign@gmail.com>
2019-01-02 22:26:20 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+	
+	if array.length == 1
+		array.join
+	elsif array.length <= 2
+		array.join(" and ")
+	else 
+		second_last = array[0..-2].join(", ")
+		second_last << ", and "
+		second_last + array.last
+	end
 end
